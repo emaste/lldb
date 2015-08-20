c3e902ab3844ac2fc828ed90a9a1e94a19863ef6
Ed Maste <emaste@freebsd.org>
2015-08-20 12:12:59 -0400

    XXX test failed due to libstdc++ issue
    llvm.org/pr20548

--- test/functionalities/data-formatter/data-formatter-skip-summary/TestDataFormatterSkipSummary.py
+++ test/functionalities/data-formatter/data-formatter-skip-summary/TestDataFormatterSkipSummary.py
@@ -19,7 +19,6 @@ class SkipSummaryDataFormatterTestCase(TestBase):
         self.buildDsym()
         self.data_formatter_commands()
 
-    @expectedFailureFreeBSD("llvm.org/pr20548") # fails to build on lab.llvm.org buildbot
     @expectedFailureWindows("llvm.org/pr24462") # Data formatters have problems on Windows
     @dwarf_test
     def test_with_dwarf_and_run_command(self):
